dbd188f18cee60c990280dcd13b5515ee9dfe7f3
TonyTandAndroid <tonytangandroid@gmail.com>
2018-11-22 18:23:56 -0500

    update picasso

--- sample/src/main/java/com/carlosdelachica/sample/adapter/view_holders/ImageEasyViewHolder.java
+++ sample/src/main/java/com/carlosdelachica/sample/adapter/view_holders/ImageEasyViewHolder.java
@@ -14,22 +14,17 @@ import butterknife.ButterKnife;
 
 public class ImageEasyViewHolder extends EasyViewHolder<ImageData> {
 
-    private final Picasso picasso;
-    @BindView(R.id.image) ImageView image;
+    @BindView(R.id.image)
+    ImageView image;
 
     public ImageEasyViewHolder(Context context, ViewGroup parent) {
-        this(context, parent, Picasso.with(context));
-    }
-
-    public ImageEasyViewHolder(Context context, ViewGroup parent, Picasso picasso) {
         super(context, parent, R.layout.image_item);
-        this.picasso = picasso;
         ButterKnife.bind(this, itemView);
     }
 
     @Override
     public void bindTo(ImageData item) {
-        picasso.load(item.getImageUrl())
+        Picasso.get().load(item.getImageUrl())
                 .placeholder(R.drawable.placeholder)
                 .into(image);
     }
